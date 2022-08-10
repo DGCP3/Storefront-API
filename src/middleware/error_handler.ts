6428a50fab36f382201420eab478c8e3a60016b1
dgcp3 <zdagmawig1@gmail.com>
2022-08-10 15:26:57 +0300

    feat(middleware): error handling middleware

---
+++ src/middleware/error_handler.ts
@@ -0,0 +1,32 @@
+import config from 'configs/default'
+import { NextFunction, Request, Response } from 'express'
+
+const { env } = config
+export class ErrorResponse extends Error {
+  constructor(message: string, public statusCode: number) {
+    super(message)
+    this.statusCode = statusCode
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+export const errorHandler = (
+  error: ErrorResponse,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (env !== 'development') {
+    res.status(error.statusCode || 500).send({ massage: error.message })
+    return
+  }
+  if (error instanceof ErrorResponse) {
+    res.status(error.statusCode).json({
+      error: error.message
+    })
+  } else {
+    res.status(500).json({
+      error: 'Oops...Something went wrong.'
+    })
+  }
+}
