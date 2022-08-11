e06db33a7dc4596411afecf5c3b399d65cf7000e
dgcp3 <zdagmawig1@gmail.com>
2022-08-11 21:17:23 +0300

    feat(routers): add route request validation

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,13 @@
 import { createAccount, login, logout } from 'controllers/auth'
 import { Router } from 'express'
 import { requestValidator } from 'middleware'
-import { loginSchema, registrationSchema } from 'schema'
+import { accountSchema, loginSchema } from 'schema'
+import { z } from 'zod'
 
 const router = Router()
 // auth & registration routes
 router.route('/login').post(requestValidator(loginSchema), login)
-router.route('/register').post(requestValidator(registrationSchema), createAccount)
+router.route('/register').post(requestValidator(z.object({ body: accountSchema })), createAccount)
 router.route('/logout').get(logout)
 
 export default router
